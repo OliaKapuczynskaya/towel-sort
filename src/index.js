a250e60c3a24a7472002a1908944900e68215d99
OliaKapuczynskaya <volyas1@gmail.com>
2021-02-21 22:16:41 +0100

    feat: add towelSort method logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+
+    let concatArray = [];
+
+    for (i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            concatArray = concatArray.concat(matrix[i]);
+        } else {
+            concatArray = concatArray.concat(matrix[i].reverse());
+        }
+    }
+
+    return concatArray;
+};
